aeaca23071c9549fafdacf2f68f9660fd2baaf6e
Stefan Scherer <scherer_stefan@icloud.com>
2018-05-11 17:34:20 +0200

    Update 2019 insider ISO to 17650

--- build_windows_2019_docker.sh
+++ build_windows_2019_docker.sh
@@ -3,5 +3,5 @@
 packer build \
   --only=vmware-iso \
   --var vhv_enable=true \
-  --var iso_url=~/packer_cache/insider/Windows_InsiderPreview_Server_vNext_en-us_17639.iso \
+  --var iso_url=~/packer_cache/insider/Windows_InsiderPreview_Server_vNext_en-us_17650.iso \
   windows_2019_docker.json
