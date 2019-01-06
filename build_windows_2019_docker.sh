232ab739b47cedc43e491197bc4e0a2c6cb3d8e5
Stefan Scherer <scherer_stefan@icloud.com>
2019-01-06 15:15:19 +0100

    Build windows_2019_docker locally with MSDN ISO

--- build_windows_2019_docker.sh
+++ build_windows_2019_docker.sh
@@ -3,6 +3,7 @@
 packer build \
   --only=vmware-iso \
   --var vhv_enable=true \
-  --var iso_url=~/downloads/17763.1.180914-1434.rs5_release_SERVER_EVAL_x64FRE_en-us.iso \
-  --var "docker_images=mcr.microsoft.com/nanoserver:sac2019 mcr.microsoft.com/windowsservercore:ltsc2019 mcr.microsoft.com/windows:ltsc2019" \
+  --var autounattend=./tmp/2019_core/Autounattend.xml \
+  --var iso_url=~/packer_cache/msdn/en_windows_server_2019_x64_dvd_4cb967d8.iso \
+  --var iso_checksum="4C5DD63EFEE50117986A2E38D4B3A3FBAF3C1C15E2E7EA1D23EF9D8AF148DD2D" \
   windows_2019_docker.json
