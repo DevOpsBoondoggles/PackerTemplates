b2cc873dd0eb94489d3f9a9ad0c3c5ebd1894b96
Stefan Scherer <scherer_stefan@icloud.com>
2019-01-22 22:22:02 +0100

    Update Windows Server Insider 18317

--- scripts/docker/install-docker.ps1
+++ scripts/docker/install-docker.ps1
@@ -1,3 +1,4 @@
+# use docker_provider:  "ee", "ce", "master" or ""
 $docker_provider = "ee"
 $docker_version = "18-09-1"
 if (Test-Path env:docker_provider) {
@@ -9,22 +10,19 @@ if (Test-Path env:docker_version) {
 
 $ProgressPreference = 'SilentlyContinue'
 if ($docker_provider -eq "ce") {
-  Set-ExecutionPolicy Bypass -scope Process
-  New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
-  Write-Output "Downloading docker $docker_version ..."
-  wget -outfile $env:TEMP\docker.zip $("https://download.docker.com/win/static/edge/x86_64/docker-{0}-ce.zip" -f $docker_version)
-  Expand-Archive -Path $env:TEMP\docker.zip -DestinationPath $env:TEMP -Force
-  copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
-  Remove-Item $env:TEMP\docker.zip
-  [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\docker", [EnvironmentVariableTarget]::Machine)
-  $env:Path = $env:Path + ";$($env:ProgramFiles)\docker"
-  Write-Output "Registering docker service ..."
-  . dockerd --register-service
+  $zip_url = $("https://download.docker.com/win/static/edge/x86_64/docker-{0}-ce.zip" -f $docker_version)
 } elseif ($docker_provider -eq "ee") {
+  $zip_url = $("https://dockermsft.blob.core.windows.net/dockercontainer/docker-{0}.zip" -f $docker_version)
+} elseif ($docker_provider -eq "master") {
+  $docker_version = "master"
+  $zip_url = "https://master.dockerproject.com/windows/x86_64/docker.zip"
+}
+
+if ($zip_url) {
   Set-ExecutionPolicy Bypass -scope Process
   New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
   Write-Output "Downloading docker $docker_version ..."
-  wget -outfile $env:TEMP\docker.zip $("https://dockermsft.blob.core.windows.net/dockercontainer/docker-{0}.zip" -f $docker_version)
+  wget -outfile $env:TEMP\docker.zip $zip_url
   Expand-Archive -Path $env:TEMP\docker.zip -DestinationPath $env:TEMP -Force
   copy $env:TEMP\docker\*.* $env:ProgramFiles\docker
   Remove-Item $env:TEMP\docker.zip
