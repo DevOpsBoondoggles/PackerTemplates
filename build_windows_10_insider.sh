f8d27f9b077207f097bdbb28daec86769fd3f678
Stefan Scherer <stefan.scherer@docker.com>
2019-12-12 07:51:34 +0100

    Update Windows Insider 19035

--- build_windows_10_insider.sh
+++ build_windows_10_insider.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-packer build --only=vmware-iso --var iso_url=~/packer_cache/insider/Windows10_InsiderPreview_EnterpriseVL_x64_en-us_19013.iso windows_10_insider.json
+packer build --only=vmware-iso --var iso_url=~/packer_cache/insider/Windows10_InsiderPreview_EnterpriseVL_x64_en-us_19035.iso windows_10_insider.json
 
 # packer build --only=vmware-iso \
-#   --var iso_url=~/packer_cache/insider/uupdump_19025.1_PROFESSIONAL_X64_EN-US.iso \
-#   --var iso_checksum=d9e98dd9187f2a251e733e3a2ca55354bd12780cb450a4eee41abbb9b1ae1058 \
+#   --var iso_url=~/packer_cache/insider/uupdump_19041.1_PROFESSIONAL_X64_EN-US.iso \
+#   --var iso_checksum=bcf500c09e2048c8bd2b710ba2b75bed9fe6ef07ea2a584599af81b4b8baa5ed \
 #   --var autounattend=tmp/10_pro/Autounattend.xml \
 #   windows_10_insider.json
