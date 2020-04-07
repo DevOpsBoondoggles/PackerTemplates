60e73980c1ba4b3fb2955e59bc4a23aa7bc7282b
Stefan Scherer <stefan.scherer@docker.com>
2020-04-07 10:28:57 +0200

    Update 19041 iso

--- build_windows_10_insider.sh
+++ build_windows_10_insider.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-packer build --only=vmware-iso --var iso_url=~/packer_cache/insider/Windows10_InsiderPreview_EnterpriseVL_x64_en-us_19035.iso windows_10_insider.json
+packer build --only=vmware-iso \
+  --var iso_url=~/packer_cache/insider/Windows10_InsiderPreview_Client_x64_en-us_19041.iso \
+  --var iso_checksum=49103c4311c63c41690b0bb445e3d3adc7904a0c29ad031169d18d4d85b66478 \
+  --var autounattend=tmp/10_pro/Autounattend.xml \
+  windows_10_insider.json
 
 # packer build --only=vmware-iso \
 #   --var iso_url=~/packer_cache/insider/uupdump_19041.1_PROFESSIONAL_X64_EN-US.iso \
